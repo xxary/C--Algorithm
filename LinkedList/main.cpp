50e5cff44e39b3c9663b508d81173bec21afa827
xxary <43515113+xxary@users.noreply.github.com>
2019-04-17 02:32:13 +0900

    Add files via upload

---
+++ LinkedList/main.cpp
@@ -0,0 +1,16 @@
+#include "linkedlist.h"
+
+int main() {
+	std::unique_ptr<List<int>> myLinkedList = std::make_unique<LinkedList<int>>();
+
+	myLinkedList->insert(1);
+	myLinkedList->insert(2);
+	myLinkedList->insert(3);
+	myLinkedList->insert(4);
+	myLinkedList->traverseList();
+
+	myLinkedList->remove(1);
+	myLinkedList->traverseList();
+
+	return 0;
+}
